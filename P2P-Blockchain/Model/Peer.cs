4e425af8c1193a2b8512695f0dad2340403206af
Sascha Worms <sworms@avans.nl>
2018-09-12 14:11:12 +0200

    initial commit

---
+++ P2P-Blockchain/Model/Peer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2P_Blockchain.Model
+{
+    public class Peer
+    {
+        public string Name { get; set; }
+        public string IPAdress { get; set; }
+    }
+}
