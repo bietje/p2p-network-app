dcaedbe6bab4feb0224356e35f011206ec3b3af2
Michel Megens <dev@bietje.net>
2018-09-12 15:51:33 +0200

    implement basic server
    Signed-off-by: Michel Megens <dev@bietje.net>

--- P2P-Blockchain/Program.cs
+++ P2P-Blockchain/Program.cs
@@ -8,10 +8,16 @@ namespace P2P_Blockchain
     {
         private static void Main(string[] args)
         {
+			Server s;
+
             Console.WriteLine("Please enter your command: ");
             Console.WriteLine("1 : Node discovery ");
             Console.WriteLine("2 : Send transaction ");
             Console.WriteLine("3 : Send Block");
+
+			s = new Server();
+			s.Start();
+
             switch (Console.ReadLine())
             {
                 case ("1"):
--- P2P-Blockchain/Server.cs
+++ P2P-Blockchain/Server.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using P2P_Blockchain.Model;
+using System.Threading;
+using System.IO;
 
 namespace P2P_Blockchain
 {
@@ -21,6 +23,51 @@ namespace P2P_Blockchain
 			} catch(Exception e) {
 				Console.WriteLine("Shit just went south.. HALP!");
 				Console.WriteLine(e.Message);
+			}
+		}
+
+		public void Start()
+		{
+			var t = new Thread(async () => {
+				while(true) {
+					var sock = await _server.AcceptTcpClientAsync().ConfigureAwait(false);
+
+					var handler = new ClientHandler();
+					Thread tp = new Thread(new ParameterizedThreadStart(handler.Run));
+					tp.Start(sock);
+				}
+			});
+
+			t.Start();
+		}
+
+		public class ClientHandler
+		{
+			private TcpClient client;
+
+			public ClientHandler()
+			{
+			}
+
+			public async void Run(object client)
+			{
+				StreamReader reader;
+				StreamWriter writer;
+
+				this.client = client as TcpClient;
+				if(client == null) {
+					Console.WriteLine("Invalid client...");
+				}
+
+				while(true) {
+					reader = new StreamReader(this.client.GetStream(), Encoding.ASCII);
+					writer = new StreamWriter(this.client.GetStream(), Encoding.ASCII);
+
+					char[] bytes = new char[2048];
+					var num =await reader.ReadAsync(bytes, 0, bytes.Length);
+					string received = new string(bytes);
+					Console.WriteLine(received);
+				}
 			}
 		}
 	}
