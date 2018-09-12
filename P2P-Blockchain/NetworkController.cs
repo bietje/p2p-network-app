2473701da174c11edc709f7b7e2bc6b9911b61bb
Michel Megens <dev@bietje.net>
2018-09-12 15:02:04 +0200

    Nieuwe dingen.
    Signed-off-by: Michel Megens <dev@bietje.net>

--- P2P-Blockchain/NetworkController.cs
+++ P2P-Blockchain/NetworkController.cs
@@ -3,23 +3,25 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using P2P_Blockchain.Model;
 
-namespace P2P_Blockchain.Model
+namespace P2P_Blockchain
 {
 	public class NetworkController
     {
-		private readonly SortedSet<Peer> clients;
+		private readonly SortedSet<Peer> peers;
+		public static  int Port = 1337;
 
 		public void AddPeer(Peer p)
 		{
-			this.clients.Add(p);
+			this.peers.Add(p);
 		}
 
 		public void RemovePeerByIp(string ip)
 		{
-			foreach(var p in clients) {
+			foreach(var p in peers) {
 				if(ip == p.IPAdress) {
-					clients.Remove(p);
+					peers.Remove(p);
 					return;
 				}
 			}
--- P2P-Blockchain/Server.cs
+++ P2P-Blockchain/Server.cs
@@ -2,6 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.Net;
+using System.Net.Sockets;
+using P2P_Blockchain.Model;
+
 namespace P2P_Blockchain
 {
+	class Server
+	{
+		private readonly TcpListener _server;
+
+		public Server()
+		{
+			this._server = new TcpListener(IPAddress.Any, NetworkController.Port);
+
+			try {
+				this._server.Start();
+			} catch(Exception e) {
+				Console.WriteLine("Shit just went south.. HALP!");
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
 }
