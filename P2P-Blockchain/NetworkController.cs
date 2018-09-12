46cf64bb8f104ffc497131d447f419e78002a90d
Michel Megens <dev@bietje.net>
2018-09-12 14:48:02 +0200

    IFJALSKDFJ
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ P2P-Blockchain/NetworkController.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2P_Blockchain.Model
+{
+	public class NetworkController
+    {
+		private readonly SortedSet<Peer> clients;
+
+		public void AddPeer(Peer p)
+		{
+			this.clients.Add(p);
+		}
+
+		public void RemovePeerByIp(string ip)
+		{
+			foreach(var p in clients) {
+				if(ip == p.IPAdress) {
+					clients.Remove(p);
+					return;
+				}
+			}
+		}
+
+
+    }
+}
---
+++ P2P-Blockchain/Server.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2P_Blockchain
+{
+}
